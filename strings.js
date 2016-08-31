27b15b019cbd328d5e36f4596ee4bd2ea1b9e247
Tommy Waters <Thomas.Waters05@gmail.com>
2016-08-30 20:53:06 -0500

    finished

--- strings.js
+++ strings.js
@@ -1,16 +1,30 @@
-function reversal() {
+var button = document.getElementById("submit");
+button.addEventListener('click', submit);
+
+
+document.getElementById("lettersOnly");
+document.getElementByid("output");
+
+
+
+function reversal(uno) {
+  return uno.split("").reverse().join("");
+}
+
+function alphabits(dos) {
+  return dos.split("").sort().join("");
 
 }
 
-function alphabits() {
-
-}
-
-function palindrome() {
-
+function palindrome(tres) {
+  return tres === reversal(tres);
 }
 
 var testString = "";
-reversal(testString);
-alphabits(testString);
-palindrome(testString);
+function submit (){
+ testString = lettersOnly.value
+ output.innerHTML = "<div>" + testString + "</div>"; //outputs normal
+ output.innerHTML += "<div>" + reversal(testString) + "</div>"; // outputs reverse
+ output.innerHTML += "<div>" + alphabits(testString) + "</div>";// outputs in alphabetical order
+ output.innerHTML += "<div>" + palindrome(testString) + "<div>";// outputs true or false if its a palindrome.
+ }
